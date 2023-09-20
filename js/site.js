310c26b58d27e08fe8f2425ef99873671d628817
A-Balan <abalan0326@gmail.com>
2023-09-20 12:29:57 -0400

    formatting

--- js/site.js
+++ js/site.js
@@ -30,3 +30,32 @@ function displayString(output) {
 
     alertBox.classList.remove('invisible');
 }
+
+function copyToClipboard() {
+    // Copy the text inside the text field
+    navigator.clipboard.writeText("abalan0326@gmail.com");
+}
+
+function showClipboardMessage() {
+    // Swal.fire({
+    //     text: "abalan0326@gmail.com",
+    //     confirmButtonText: "Copy"
+    // }).then(function (result) {
+    //     copyToClipboard();
+    // });
+
+    Swal.fire({
+        html: '<br />abalan0326@gmail.com ',
+        showCloseButton: true,
+        focusConfirm: false,
+        confirmButtonText: '<i class="bi bi-clipboard"></i> Copy',
+    }).then(function (result) {
+        copyToClipboard();
+
+        if (result.value === true) {
+            Swal.fire({
+                html: "<br />Copied to Clipboard!"
+            })
+        }
+    })
+}
